5528355036701cda9b051d622e44d29a25badefb
Prithvi <34770840+xPrithvi@users.noreply.github.com>
2018-09-08 11:37:51 +0400

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,1 +1,6 @@
+from Client import LExpressProperty
+
+AGENT = LExpressProperty.Agent()
+DATA = AGENT.get(payment = "buy", property_type = "house", sort_by = "most expensive", pages = 5)
+print(DATA)
 
